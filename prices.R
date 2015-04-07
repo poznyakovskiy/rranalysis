cf61394a74ab611a2b667e70ac00eff2d8420d14
poznyakovskiy <anton.poznyakovskiy@gmail.com>
2015-04-07 03:53:07 +0200

    Compactified data loading, added weighted price plotting

--- prices.R
+++ prices.R
@@ -1,4 +1,13 @@
-data <- read.csv ("data.csv", header = FALSE)
-names (data) = c ("name", "location", "amount", "distance", "price", "price.adjusted")
-data$name <- as.character (data$name)
-data$location <- as.character (data$location)
+library (ggplot2)
+library (ENmisc)
+
+data <- read.csv ("data.csv",
+                  header = FALSE,
+                  col.names = c ("name", "location", "amount", "distance", "ppu", "ppu.adjusted"),
+                  colClasses = c ("character", "character", "numeric", "numeric", "numeric", "numeric"))
+
+bpstats <- wtd.boxplot (data$ppu, data$amount)$stats
+plot <- ggplot (data, aes (x = c(1), y = ppu, weight = amount)) +
+        geom_boxplot(outlier.size = 0) +
+        coord_cartesian (ylim = c(0, bpstats[5] * 1.05))
+print (plot)
